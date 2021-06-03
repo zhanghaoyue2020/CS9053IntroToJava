e2f12faa073417fe1c7ced5860f5ee5024cbe0ca
zhanghaoyue2020 <85269000+zhanghaoyue2020@users.noreply.github.com>
2021-06-03 01:16:08 -0400

    Add files via upload

---
+++ PartI/MyInitials.java
@@ -0,0 +1,11 @@
+public class MyInitials {
+	public static void main(String[] args) {			
+		System.out.println("ZZZZZ  H   H  Y   Y");
+		System.out.println("    Z  H   H   Y Y ");
+		System.out.println("   Z   H   H    Y  ");
+		System.out.println("  Z    HHHHH    Y  ");
+		System.out.println(" Z     H   H    Y  ");
+		System.out.println("Z      H   H    Y  ");
+		System.out.println("ZZZZZ  H   H    Y  ");
+	}
+}
